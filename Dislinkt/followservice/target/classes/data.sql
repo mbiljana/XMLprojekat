c7b78f59412cdabdebe47a6d4873bc2682180691
mbiljana <mbiljana99@gmail.com>
2022-05-07 18:08:35 +0200

    files

---
+++ Dislinkt/followservice/target/classes/data.sql
@@ -0,0 +1,10 @@
+insert into profile(id,USERNAME,is_blocked,is_public)
+values(1,'bika',false,true);
+insert into profile(id,USERNAME,is_blocked,is_public)
+values(2,'maki',false,true);
+
+insert into follow_request(id,REQ_DATE,profile1_id)
+values (20,'2022-02-02',1);
+
+insert into connection(id)
+values (55);
