6720079fabadafa920322d540aeab8aecd7bbdc5
AnamarijaD <anamarijadolovac@gmail.com>
2022-05-08 21:02:27 +0200

    Fixed updateService

--- Dislinkt/followservice/src/main/java/com/example/followservice/repository/FollowRequestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.followservice.repository;
-
-import com.example.followservice.model.FollowRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FollowRequestRepository extends JpaRepository<FollowRequest,Long> {
-
-}
