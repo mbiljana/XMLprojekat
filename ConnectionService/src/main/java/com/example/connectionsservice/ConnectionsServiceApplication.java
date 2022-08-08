5885d3de67f14cc818e552be9162d45a6b0a8a11
mbiljana <mbiljana99@gmail.com>
2022-08-08 22:22:12 +0200

    mongo db connection

--- ConnectionService/src/main/java/com/example/connectionsservice/ConnectionsServiceApplication.java
+++ ConnectionService/src/main/java/com/example/connectionsservice/ConnectionsServiceApplication.java
@@ -1,10 +1,17 @@
 package com.example.connectionsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ConnectionsServiceApplication {
+
+
+    @Autowired
+    InitialData initialData;
 
     public static void main(String[] args) {
         SpringApplication.run(ConnectionsServiceApplication.class, args);
---
+++ ConnectionService/src/main/java/com/example/connectionsservice/InitialData.java
@@ -0,0 +1,17 @@
+package com.example.connectionsservice;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class InitialData {
+
+
+    //services
+
+    @PostConstruct
+    public void init(){
+
+    }
+}
