e1fb12d038c1ee05bb7db7760f882b6e8f362ac1
aleksandra-b99 <aleksandra.borisavljevic99@gmail.com>
2022-08-14 12:47:05 +0200

    delete security service from banch fun2.2

--- SecurityService/src/main/java/com/example/securityservice/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.securityservice.Repository;
-
-import com.example.securityservice.Model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface UserRepository extends MongoRepository<User,String> {
-    User findByKorisnicko(String username);
-}
