a04adf1333630b64de4a7e9a039c90aa357565c7
aleksandra-b99 <aleksandra.borisavljevic99@gmail.com>
2022-08-23 13:41:33 +0200

    added data in database

--- Agent/src/main/java/com/example/Agent/InitialData.java
+++ Agent/src/main/java/com/example/Agent/InitialData.java
@@ -8,8 +8,15 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.example.Agent.model.Address;
+import com.example.Agent.model.Company;
+import com.example.Agent.model.Post;
 import com.example.Agent.model.Role;
 import com.example.Agent.model.User;
+import com.example.Agent.repository.AddressRepository;
+import com.example.Agent.repository.CompanyRepository;
+import com.example.Agent.repository.PostRepository;
+import com.example.Agent.service.AddressService;
 import com.example.Agent.service.RoleService;
 import com.example.Agent.service.UserService;
 
@@ -19,11 +26,19 @@ public class InitialData {
 
     private final UserService userService;
     private final RoleService roleService;
+    private final AddressRepository addressRepository;
+    private final CompanyRepository companyRepository;
+    private final PostRepository postRepository;
 
     @Autowired
-    public InitialData(  RoleService roleService, UserService userService){
+    public InitialData(  RoleService roleService, UserService userService,
+    		AddressRepository addressRepository,CompanyRepository companyRepository,
+    		PostRepository postRepository){
         this.userService = userService;
         this.roleService = roleService;
+        this.addressRepository=addressRepository;
+        this.companyRepository=companyRepository;
+        this.postRepository=postRepository;
     }
 
 
@@ -52,8 +67,36 @@ public class InitialData {
         userService.saveUser(u2);
         userService.saveUser(u3);
         userService.saveUser(u4);
-
-
-
+        
+        Address a1=new Address((long)1, "Maksima Gorkog 1", "Srbija", "Novi Sad");
+        Address a2=new Address((long)2, "Radnicka 1", "Srbija", "Subotica");
+        Address a3=new Address((long)3, "Lasla Gaja 3", "Srbija", "Beograd");
+        
+        addressRepository.save(a1);
+        addressRepository.save(a2);
+        addressRepository.save(a3);
+        
+        List<String> preconditions1=new ArrayList<>();
+		preconditions1.add("c++");
+		preconditions1.add("object-oriented programming");
+		preconditions1.add("previous experience");
+		
+		List<String> preconditions2=new ArrayList<>();
+		preconditions2.add("Python");
+		preconditions2.add("object-oriented programming");
+		preconditions2.add("previous experience");
+        
+        
+        
+        Company c1=new Company((long)1, "M&I", "M&I Systems, Co. is a privately held company established in 1991 as Management & Information Systems Company, based in Novi Sad. In almost three decades of operation, M&I Systems, Co. has emerged as the leader when it comes to the development and implementation of information solutions within various industries. Our successful work includes the development of ERP software solutions, business intelligence systems, banking software, specialized software solutions for various industries, as well as the provision of business process management, optimization, and performance management services.", a1, "m&icorporation", "m&i@gmail.com", "34832482", "/assets/companyPicture/m&i.png", u2);
+        this.companyRepository.save(c1);
+        
+        Post p1=new Post((long)1, "Java job offer", "Our company looking for junior java developer, experiance is not necessary.",preconditions1,"Java senior developer", "/assets/postPicture/post1.jpeg", 0,0,c1);
+        Post p2=new Post((long)2, "Python job offer", "We are looking for senior Python developer, with very good experiance in programing and also communication skills",preconditions2,"Python junior developer","/assets/postPicture/post2.jpeg", 0,0,c1);
+        
+        postRepository.save(p1);
+        postRepository.save(p2);
+        
+        
     }
 }
